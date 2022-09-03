0460c4611447f24bbbe0e29c4ee692b5269f0783
Marina Landisberg <mlandisberg@gmail.com>
2022-09-02 18:18:03 -0700

    vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password
 from django.contrib import messages
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
+from vendor.forms import VendorForm
 
 def registerUser(request):
 	if request.method == 'POST':
@@ -40,3 +41,42 @@ def registerUser(request):
 		'form': form,
 	}
 	return render(request, 'accounts/registerUser.html', context)
+
+def registerVendor(request):
+	if request.method == 'POST':
+		form = UserForm(request.POST)
+		v_form = VendorForm(request.POST, request.FILES)
+		if form.is_valid() and v_form.is_valid():
+			first_name = form.cleaned_data['first_name']
+			last_name = form.cleaned_data['last_name']
+			password = form.cleaned_data['password']
+			username = form.cleaned_data['username']
+			email	 = form.cleaned_data['email']
+			user = User.objects.create_user(
+				first_name=first_name, 
+				last_name=last_name, 
+				username=username, 
+				email=email, 
+				password= password 
+			)
+			user.role = User.VENDOR
+			user.save()
+			vendor = v_form.save(commit=False)
+			vendor.user = user
+			user_profile = UserProfile.objects.get(user=user)
+			vendor.user_profile = user_profile
+			vendor.save()
+			messages.success(request, 'Your account has been registered successfully! Please wait for the approval.')
+			return redirect('registerVendor')
+		else:
+			print('invalid form')
+			print(form.errors)
+	else:
+		form = UserForm()
+		v_form = VendorForm()
+
+	context = {
+		'form': form,
+		'v_form': v_form	
+	}
+	return render(request, 'accounts/registerVendor.html', context)
--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 		'django.contrib.gis',
 
 		'accounts.apps.AccountsConfig',
+		'vendor.apps.VendorConfig',
 ]
 
 MIDDLEWARE = [
